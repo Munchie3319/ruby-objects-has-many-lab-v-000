72895d4878523a317b9b1124a5838c103aee3b5b
rosa lee <rosalee0116@gmail.com>
2020-04-10 18:56:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,21 +3,19 @@ class Song
 
   @@all = []
 
-  def initialize(name, artist=nil)
+  def initialize(name)
     @name = name
-    @artist = artist
-    @@all << self
+    save 
   end
 
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
+  def save
+    @@all << self 
+  end 
 
   def self.all
     @@all
   end
+
+  def artist_name
+    self.artist == nil ? nil :self.artist.name 
 end
